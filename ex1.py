3f4851820ba353248b7707207c9ea8c3122d71a9
David Gus Shields <salacio@gmail.com>
2014-02-17 20:30:56 -0500

    new file:   ex1.py 	new file:   ex2.py 	new file:   ex3.py

---
+++ ex1.py
@@ -0,0 +1,9 @@
+#Exercise 1
+print "Hello World"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather 'not'."
+print 'I "said" do not touch this.'
+print "Python is fun!"
