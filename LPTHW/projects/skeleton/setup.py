de876a6aac495beefed8cce2c6c5fb2149de138a
salacio <salacio@gmail.com>
2014-02-26 15:25:38 -0500

    new file:   setup.py 	new file:   tests/NAME_tests.py

---
+++ LPTHW/projects/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from disutils.core import setup
+
+config = {
+        'description':'My Project',
+        'author':'David G Shields',
+        'url':'https://github.com/salacio',
+        'download_url':'Where to download it.',
+        'author_email':'salcio@gmail.com',
+        'version':'0.1',
+        'install_requires':['nose'],
+        'packages':['NAME'],
+        'scripts':[],
+        'name':'projectname'
+        }
+
+setup(**config)
