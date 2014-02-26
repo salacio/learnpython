80c0a63f44bc53f44406e0da9571db8322ce3a23
salacio <salacio@gmail.com>
2014-02-26 00:41:41 -0500

    modified:   ex44c.py

--- LPTHW/ex44c.py
+++ LPTHW/ex44c.py
@@ -1,10 +1,10 @@
 class Parent(object):
 
     def altered(self):
-        print "PARENT altered()"
+        print("PARENT altered()")
 
 class Child(Parent):
 
     def altered(self):
-        print "CHILD, BEFORE PARENT altered()"
+        print("CHILD, BEFORE PARENT altered()")
         super(
