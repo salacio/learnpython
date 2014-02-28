90186d95830a40ef0a8ad9a5148a8a2a06df98f4
salacio <salacio@gmail.com>
2014-02-27 23:12:24 -0500

    new file:   store.py

---
+++ codecademy/store.py
@@ -0,0 +1,26 @@
+shopping_list = ["banana", "orange", "apple"]
+
+prices= {
+        "banana" : 4,
+        "apple" : 2,
+        "orange" : 1.5,
+        "pear" : 3
+        }
+
+stock = {
+        "banana" : 6,
+        "apple" : 0,
+        "orange" : 32,
+        "pear" : 15
+        }
+
+"""for key in prices:
+    print("%s\nprice: %s\nstock: %s\n"
+            % (key, prices[key], stock[key]))
+"""
+def compute_bill(food):
+    total = 0
+    for item in food:
+        total += food
+    return total
+compute_bill(shopping_list)
